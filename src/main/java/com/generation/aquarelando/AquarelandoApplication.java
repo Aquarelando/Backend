c2559d92a113b083eccc1359cdd7ab35c9635008
BiaAkemi <beancaakemi@gmail.com>
2023-12-13 11:59:57 -0300

    Criando Projeto Spring

---
+++ src/main/java/com/generation/aquarelando/AquarelandoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.aquarelando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquarelandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquarelandoApplication.class, args);
+	}
+
+}
