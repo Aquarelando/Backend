5fccab9131bc01601b25ea882147759c44f2fe40
BiaAkemi <beancaakemi@gmail.com>
2023-12-15 10:49:08 -0300

    @BiaAkemi adicionando requisão criando e atualizando categoria

--- src/main/java/com/generation/aquarelando/controller/CategoriaController.java
+++ src/main/java/com/generation/aquarelando/controller/CategoriaController.java
@@ -8,11 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.aquarelando.model.Categoria;
 import com.generation.aquarelando.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categoria")
@@ -22,12 +27,14 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	// Buscando todas as categorias
 	@GetMapping 
 	public ResponseEntity<List<Categoria>>getAll(){
 		return ResponseEntity.ok(categoriaRepository.findAll());
 		
 	}
 	
+	// Procurando por id
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria>getById(@PathVariable Long id){
 		return categoriaRepository.findById(id)
@@ -35,11 +42,27 @@ public class CategoriaController {
 		.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 		
 	}
-				
+	
+	// Procurando por nome
 	@GetMapping("/nome/{nome}")
 	public ResponseEntity<List<Categoria>> getByNome(@PathVariable String nome){
 		return ResponseEntity.ok(categoriaRepository.findAllByNomeContainingIgnoreCase(nome));
 	}
 	
+	// Criando categoria
+	@PostMapping
+	public ResponseEntity<Categoria> post (@Valid @RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(categoriaRepository.save(categoria));
+	}
+	
+	//Atualizando categoria
+	@PutMapping
+	public ResponseEntity<Categoria> put (@Valid @RequestBody Categoria categoria) {
+		return categoriaRepository.findById(categoria.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+				.body(categoriaRepository.save(categoria)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
 }
