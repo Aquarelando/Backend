e3c361301a3ff981edf2d5d95d50637b761fdc47
littlledan <daniel799.dl@gmail.com>
2023-12-15 11:07:27 -0300

    @littlledan Adicionando requisição delete e mapping

--- src/main/java/com/generation/aquarelando/controller/CategoriaController.java
+++ src/main/java/com/generation/aquarelando/controller/CategoriaController.java
@@ -1,18 +1,22 @@
 package com.generation.aquarelando.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.aquarelando.model.Categoria;
 import com.generation.aquarelando.repository.CategoriaRepository;
@@ -20,7 +24,7 @@ import com.generation.aquarelando.repository.CategoriaRepository;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categorias")
 @CrossOrigin(origins = "*" ,allowedHeaders = "*")
 public class CategoriaController {
 	
@@ -50,19 +54,31 @@ public class CategoriaController {
 	}
 	
 	// Criando categoria
-	@PostMapping
+	@PostMapping("/new")//localhost:8080/categoria/new
 	public ResponseEntity<Categoria> post (@Valid @RequestBody Categoria categoria) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(categoriaRepository.save(categoria));
 	}
 	
 	//Atualizando categoria
-	@PutMapping
+	@PutMapping("/edit")//localhost:8080/categoria/edit
 	public ResponseEntity<Categoria> put (@Valid @RequestBody Categoria categoria) {
 		return categoriaRepository.findById(categoria.getId())
 				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
 				.body(categoriaRepository.save(categoria)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	//Deletando categoria
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/delete/{id}")//localhost:8080/categoria/delete
+	public void delete(@PathVariable Long id) {
+		Optional<Categoria> categoria =categoriaRepository.findById(id);
+		
+		if(categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		categoriaRepository.deleteById(id);
+			
 	}
 
 }
