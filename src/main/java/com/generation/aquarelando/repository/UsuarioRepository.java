8dcedfde74e4f71f51bc171867723d4b012f5fec
Renan Nunes <rafaelnc22@gmail.com>
2024-01-05 10:38:23 -0300

    @Raafa22 Atualizando código e testando swagger

--- src/main/java/com/generation/aquarelando/repository/UsuarioRepository.java
+++ src/main/java/com/generation/aquarelando/repository/UsuarioRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.aquarelando.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario , Long>{
+		
 	
-	 public Optional<Usuario> findByEmail(String email);
+	public Optional<Usuario> findByEmail(String email);
 
 }
